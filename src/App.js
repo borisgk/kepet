d40b0d2c36fc541110cbcc9b7e6ba164903fe346
gvrsrg <gvrsrg@gmail.com>
2023-09-19 20:42:43 +0300

    event calendar - 2 lines

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
 import Menubar from './components/Menubar';
-import "react-big-calendar/lib/css/react-big-calendar.css";
 import Reminders from './components/Reminders';
 
 function App() {
